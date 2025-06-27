2327d30bf094cd92f5afee945378688689e897ad
luisOliveira <oliveirammluis@gmail.com>
2025-06-27 22:41:42 +0100

    Enhance profile image style and responsiveness on Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,11 +3,21 @@ import Footer from '../components/Footer';
 import '../styles/Home.css';
 
 const helloTranslations = [
-  { language: 'English', text: 'Hello World' },
-  { language: 'Portuguese', text: 'Olá Mundo' },
-  { language: 'Spanish', text: 'Hola Mundo' },
-  { language: 'French', text: 'Bonjour le Monde' },
-  { language: 'Italian', text: 'Ciao Mondo' },
+  { country: 'Portugal', text: '🇵🇹 Olá Mundo' },
+  { country: 'Espanha', text: '🇪🇸 Hola Mundo' },
+  { country: 'França', text: '🇫🇷 Bonjour le Monde' },
+  { country: 'Itália', text: '🇮🇹 Ciao Mondo' },
+  { country: 'Inglaterra', text: '🇬🇧 Hello World' },
+  { country: 'Luxemburgo', text: '🇱🇺 Moien Welt' },
+  { country: 'Áustria', text: '🇦🇹 Hallo Welt' },
+  { country: 'República Checa', text: '🇨🇿 Ahoj světe' },
+  { country: 'Polónia', text: '🇵🇱 Witaj świecie' },
+  { country: 'Alemanha', text: '🇩🇪 Hallo Welt' },
+  { country: 'Hungria', text: '🇭🇺 Helló Világ' },
+  { country: 'Tailândia', text: '🇹🇭 สวัสดีชาวโลก' },
+  { country: 'Maldivas', text: '🇲🇻 ޝަންގަނޑު ޖަން' },
+  { country: 'EUA (Nova Iorque)', text: '🇺🇸 Hello World' },
+  { country: 'Cuba', text: '🇨🇺 Hola Mundo' },
 ];
 
 function Home() {
@@ -25,7 +35,11 @@ function Home() {
     <div className="home-container">
       <div className="content">
         <div className="image-container mb-20">
-          <img src="/images/my-photo.jpg" alt="Luis Oliveira" />
+          <img
+            src="/images/my-photo.png"
+            alt="Luis Oliveira"
+            className="image-container-img"
+          />
         </div>
         <div className="text-container">
           <h1>{helloTranslations[currentHelloIndex].text}</h1>
